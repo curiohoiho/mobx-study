3d8f4e1e55d6183fdbbef76feca696db66c000fd
curio hoiho <curiohoiho@users.noreply.github.com>
2017-02-14 17:42:07 -0800

    atom.ts

--- src/core/atom.ts
+++ src/core/atom.ts
@@ -1,4 +1,13 @@
-import { IObservable } from './observable';
+import { globalState } from './globalstate';
+import { 
+  IObservable,
+  propagateChanged,
+  reportObserved,
+  startBatch,
+  endBatch } from './observable';
+import { IDerivationState } from './derivation';
+import { createInstanceofPredicate, noop, getNextId } from '../utils/utils';
+
 
 export interface IAtom extends IObservable
 {
@@ -16,3 +25,28 @@ export interface IAtom extends IObservable
    */
 
 } // IAtom
+
+export class BaseAtom implements IAtom {
+
+  // for effective unobserving.  BaseAtom has true, 
+  // for extra opimization, so its onBecomeUnobserved never gets called, 
+  // because it's not needed
+  isPendingUnobservation = true;
+  observers = [];
+  observersIndexes = {};
+
+  diffValue = 0;
+  lastAccessedBy = 0;
+  lowestObserverState = IDerivationState.NOT_TRACKING;
+
+  /**
+	 * Create a new atom. For debugging purposes it is recommended to give it a name.
+	 * The onBecomeObserved and onBecomeUnobserved callbacks can be used for resource management.
+	 */
+  
+
+
+} // class BaseAtom
+
+
+export const isAtom = createInstanceofPredicate("Atom", BaseAtom);
